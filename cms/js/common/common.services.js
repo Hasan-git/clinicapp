5c5ef6fcf54c7950b978d828254cc7eb17c678ab
Hasan-git <hasan.ri25@gmail.com>
2017-06-18 21:28:30 +0300

    ignore services.js

--- cms/js/common/common.services.js
+++ cms/js/common/common.services.js
@@ -8,6 +8,6 @@
         {
             //serverPath: "http://192.168.1.110:63392"
             //serverPath: "http://localhost:63392"
-            serverPath: "http://haisam:63392"
+            //serverPath: "http://haisam:63392"
         });
 }());
